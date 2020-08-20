c1730f2d048e684897e3e3123766ba655f427bfb
jin <choiuijin1125@gmail.com>
2020-08-20 16:02:37 +0900

    upload template

---
+++ src/views/Starter.vue
@@ -0,0 +1,28 @@
+<template>
+        <div class="position-relative">
+            <!-- shape Hero -->
+            <section class="section-shaped my-0">
+                <div class="shape shape-style-1 shape-default shape-skew">
+                    <img v-lazy="'img/icons/common/main.jpg'" class="card-img">
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h1 class="display-3  text-white">NIA 랜드마크 데이터셋
+                                    <span>500만장의 랜드마크 데이터셋 구축</span>
+                                </h1>
+                                <p class="lead  text-white">50개 도시, 500개 랜드마크, 이미지 200장</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- 1st Hero Variation -->
+        </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
