b9e05479e123ce8b98d9bdf3ce2ad4b1c8868ccc
Choi Uijin <choiuijin1125@gmail.com>
2020-10-24 18:38:18 +0900

    upload Dropzone

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,6 +1,6 @@
-/<template>
-  <div class="position-relative">
-    <section class="section pt-150 section-components">
+<template>
+  <div id ="app" class="position-relative">
+    <section class="section pt-150 pb-0 section-components">
       <div class="container mb-5">
         <h2 class="h2 text-lg-center font-weight-bold mb-5">시도해보세요 !</h2>
         <div class="container">
@@ -31,12 +31,96 @@
       </div>
     </section>
     <section>
-        <div class="ct-example-row text-center">
-    <div class="row">
-      <div class="col-sm-12 text-center">
-      <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions" v-on:vdropzone-sending=fileSelect></vue-dropzone>
+        <div class="container">
+    <div class="row text-center">
+      <div class="col-12 ">
+      <vue-dropzone class="col-12" ref="dropzone" id="drop1" :options="dropzoneOptions" v-on:vdropzone-sending=fileSelect></vue-dropzone>
+                <table v-show="probSeen" class="table-sm table mt-3">
+              <thead>
+                  <tr>
+                      <th>top-5</th>
+                      <th>name</th>
+                      <th>prob</th>
+                      
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>top-1</td>
+                      <td>{{pred.class_name[0]}}</td>
+                      <td>
+                        <div class="progress-wrapper">
+                          <div class="progress-primary">
+                              <div class="progress-percentage">
+                                <span>{{pred.probs[0]}}%</span>
+                                </div>
+                                </div>
+                                <div class="progress" style="height: 8px;">
+                                  <div role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" class="progress-bar bg-primary" :style="{'width': `${parseInt(100 * pred.probs[0])}%`}"></div></div></div>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>top-2</td>
+                      <td>{{pred.class_name[1]}}</td>
+                      <td>
+                        <div class="progress-wrapper">
+                          <div class="progress-primary">
+                              <div class="progress-percentage">
+                                <span>{{pred.probs[1]}}%</span>
+                                </div>
+                                </div>
+                                <div class="progress" style="height: 8px;">
+                                  <div role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" class="progress-bar bg-primary" :style="{'width': `${parseInt(100 * pred.probs[1])}%`}"></div></div></div>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>top-3</td>
+                      <td>{{pred.class_name[2]}}</td>
+                      <td>
+                        <div class="progress-wrapper">
+                          <div class="progress-primary">
+                              <div class="progress-percentage">
+                                <span>{{pred.probs[2]}}%</span>
+                                </div>
+                                </div>
+                                <div class="progress" style="height: 8px;">
+                                  <div role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" class="progress-bar bg-primary" :style="{'width': `${parseInt(100 * pred.probs[2])}%`}"></div></div></div>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>top-4</td>
+                      <td>{{pred.class_name[3]}}</td>
+                      <td>
+                        <div class="progress-wrapper">
+                          <div class="progress-primary">
+                              <div class="progress-percentage">
+                                <span>{{pred.probs[3]}}%</span>
+                                </div>
+                                </div>
+                                <div class="progress" style="height: 8px;">
+                                  <div role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" class="progress-bar bg-primary" :style="{'width': `${parseInt(100 * pred.probs[3])}%`}"></div></div></div>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>top-5</td>
+                      <td class="align-middle">{{pred.class_name[4]}}</td>
+                      <td>
+                        <div class="progress-wrapper">
+                          <div class="progress-primary">
+                              <div class="progress-percentage">
+                                <span>{{pred.probs[4]}}%</span>
+                                </div>
+                                </div>
+                                <div class="progress" style="height: 8px;">
+                                  <div role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" class="progress-bar bg-primary" :style="{'width': `${parseInt(100 * pred.probs[4])}%`}"></div></div></div>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      <button class="mt-4 btn btn-primary" @click="removeAllFiles">Reset</button>
       </div>
-      <div class="col-sm-4"><span>{{pred.class_name[0]}}</span><button v-on:vdropzone-removed-file=removeImage>reset</button></div>
+      <div class="col-sm-4">
+      </div>
       
     </div>
 </div>
@@ -48,32 +132,39 @@
 <script>
   import Vue from "vue";
   import axios from "axios";
-  import vue2Dropzone from 'vue2-dropzone'
+  import vueDropzone from 'vue2-dropzone'
   import 'vue2-dropzone/dist/vue2Dropzone.min.css'
   export default {
     components:{
-        vueDropzone: vue2Dropzone
+        vueDropzone
     },
     data() {
       return {
-        pred: {'class_name': ''},
+        pred: {'class_name': '',
+               'probs': ''},
+        probSeen: false,
         dropzoneOptions: {
           url: 'https://httpbin.org/post',
           thumbnailWidth: 640,
           thumbnailHeight: 500,
           maxFilesize: 4,
           maxFiles: 1,
-          headers: { "My-Awesome-Header": "header value" }
+          headers: { "My-Awesome-Header": "header value" },
+          addRemoveLinks: true
       }
       };
     },
     methods: {
-      removeImage(file) {
-          this.$refs.myVueDropzone.removeFile(file)
+      removeAllFiles() {
+          this.$refs.dropzone.removeAllFiles(true)
+          var vm = this;
+          vm.probSeen = false;
+          vm.pred = {'class_name': '',
+                     'probs': ''}
         },
-
       fileSelect(file, xhr, formData) {
-        var vm = this;  
+        var vm = this; 
+        vm.probSeen = true; 
         formData.append('file', file);
         axios
           .post("http://15.165.113.21:5000/fileUpload", formData, {
